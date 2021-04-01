1abaea23da4e8f1a04da3dd2f1e8d33c3c7a1f5c
ludfyrahman <ludfyr@gmail.com>
2021-04-01 16:28:29 +0700

    tweak camera opencv

--- InstallOpenCV/app/src/main/java/com/example/installopencv/MainActivity.java
+++ InstallOpenCV/app/src/main/java/com/example/installopencv/MainActivity.java
@@ -4,33 +4,147 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
-    private static String TAG = "MainActivity";
-    static  {
-        if (OpenCVLoader.initDebug()) {
-            Log.d(TAG, "OpenCV sukses di install");
-        }else {
-            Log.d(TAG, "OpenCV gagal di install");
-        }
-    }
-
-    static {
-        System.loadLibrary( "native-lib");
-    }
+//    private static String TAG = "MainActivity";
+//    static  {
+//        if (OpenCVLoader.initDebug()) {
+//            Log.d(TAG, "OpenCV sukses di install");
+//        }else {
+//            Log.d(TAG, "OpenCV gagal di install");
+//        }
+//    }
+//
+//    static {
+//        System.loadLibrary( "native-lib");
+//    }
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+    boolean startCanny = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.CameraView);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
+
+
+        //System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                super.onManagerConnected(status);
+
+                switch(status){
+
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+
+
+            }
+
+        };
     }
 
     public native String stringFromJNI();
+    public void Canny(View Button){
+
+        if (startCanny == false){
+            startCanny = true;
+        }
+
+        else{
+
+            startCanny = false;
+
+
+        }
+    }
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        Mat frame = inputFrame.rgba();
+
+        if (startCanny == true) {
+
+            Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGBA2GRAY);
+            Imgproc.Canny(frame, frame, 100, 80);
+
+        }
+
+
+
+        return frame;
+    }
+
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(),"There's a problem, yo!", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            baseLoaderCallback.onManagerConnected(baseLoaderCallback.SUCCESS);
+        }
+
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase!=null){
+
+            cameraBridgeViewBase.disableView();
+        }
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
 }
