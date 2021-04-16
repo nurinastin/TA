f4d5c7289591744decd82f33ffc52837f724974a
ludfyrahman <ludfyr@gmail.com>
2021-04-16 18:05:51 +0700

    routint

--- InstallOpenCV/app/src/main/java/com/example/installopencv/Home.java
+++ InstallOpenCV/app/src/main/java/com/example/installopencv/Home.java
@@ -2,13 +2,24 @@ package com.example.installopencv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Home extends AppCompatActivity {
-
+    FloatingActionButton tambah;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        tambah = findViewById(R.id.tambah);
+        tambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+            }
+        });
     }
 }
--- InstallOpenCV/app/src/main/java/com/example/installopencv/Splash.java
+++ InstallOpenCV/app/src/main/java/com/example/installopencv/Splash.java
@@ -2,6 +2,7 @@ package com.example.installopencv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        final Intent i = new Intent(this, Home.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+//        memulai timer splash screen
+        timer.start();
     }
 }
