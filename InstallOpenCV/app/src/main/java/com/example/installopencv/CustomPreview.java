c601f9f5f72b2ff7d9d199e5d4cb42b1491ff7e0
awaludin12269 <awaludinnurmuhammad201@gmail.com>
2021-07-14 11:14:58 +0700

    fix camera

--- InstallOpenCV/app/src/main/java/com/example/installopencv/CustomPreview.java
+++ InstallOpenCV/app/src/main/java/com/example/installopencv/CustomPreview.java
@@ -31,7 +31,9 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -42,37 +44,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-public class CustomPreview extends AppCompatActivity implements SurfaceHolder.Callback, Handler.Callback {
-    static final String TAG = "CamTest";
+public class CustomPreview extends AppCompatActivity  {
     static final int MY_PERMISSIONS_REQUEST_CAMERA = 1242;
-    private static final int MSG_CAMERA_OPENED = 1;
-    private static final int MSG_SURFACE_READY = 2;
-    private final Handler mHandler = new Handler(this);
-    SurfaceView mSurfaceView;
-    SurfaceHolder mSurfaceHolder;
-    CameraManager mCameraManager;
-    String[] mCameraIDsList;
-    CameraDevice.StateCallback mCameraStateCB;
-    CameraDevice mCameraDevice;
     TextView open, flip;
-    CameraCaptureSession mCaptureSession;
-    boolean mSurfaceCreated = true;
-    boolean mIsCameraConfigured = false;
-    private Surface mCameraSurface = null;
     ImageView cover;
     int SELECT_IMAGE = 1;
     private Uri contentURI;
     Bitmap bitmap;
     int flipn = 0;
+    Camera camera;
+    FrameLayout view;
+    ShowCamera show;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_preview);
         getWindow().setFormat(PixelFormat.UNKNOWN);
-        this.mSurfaceView = (SurfaceView) findViewById(R.id.view);
-        this.mSurfaceHolder = this.mSurfaceView.getHolder();
-        this.mSurfaceHolder.addCallback(this);
         cover = findViewById(R.id.cover);
         open = findViewById(R.id.open);
         flip = findViewById(R.id.flip);
@@ -105,46 +93,19 @@ public class CustomPreview extends AppCompatActivity implements SurfaceHolder.Ca
             }
         });
         try {
-//            if(contentURI==null){
             Intent data = getIntent();
-//            Log.d("file", data.getStringExtra("file"));
             if(!data.getStringExtra("file").equals("")){
                 loadfile();
             }
-//            }
         } catch (IOException e) {
             Log.d("pesanE", e.getMessage());
             e.printStackTrace();
         }
-        this.mCameraManager = (CameraManager) this.getSystemService(Context.CAMERA_SERVICE);
-        try {
-            mCameraIDsList = this.mCameraManager.getCameraIdList();
-            for (String id : mCameraIDsList) {
-                Log.v(TAG, "CameraID: " + id);
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
+        camera = Camera.open();
+        view = findViewById(R.id.view);
+        show = new ShowCamera(this, camera);
+        view.addView(show);
 
-        mCameraStateCB = new CameraDevice.StateCallback() {
-            @Override
-            public void onOpened(CameraDevice camera) {
-                Toast.makeText(getApplicationContext(), "onOpened", Toast.LENGTH_SHORT).show();
-
-                mCameraDevice = camera;
-                mHandler.sendEmptyMessage(MSG_CAMERA_OPENED);
-            }
-
-            @Override
-            public void onDisconnected(CameraDevice camera) {
-                Toast.makeText(getApplicationContext(), "onDisconnected", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onError(CameraDevice camera, int error) {
-                Toast.makeText(getApplicationContext(), "onError", Toast.LENGTH_SHORT).show();
-            }
-        };
     }
     @Override
     public void onBackPressed()
@@ -194,88 +155,7 @@ public class CustomPreview extends AppCompatActivity implements SurfaceHolder.Ca
         cover.setAlpha(127);
         streamIn.close();
     }
-    @Override
-    protected void onStart() {
-        super.onStart();
 
-        //requesting permission
-        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-
-            } else {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
-                Toast.makeText(getApplicationContext(), "request permission", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            Toast.makeText(getApplicationContext(), "PERMISSION_ALREADY_GRANTED", Toast.LENGTH_SHORT).show();
-            try {
-                mCameraManager.openCamera(mCameraIDsList[0], mCameraStateCB, new Handler());
-            } catch (CameraAccessException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        try {
-            if (mCaptureSession != null) {
-                mCaptureSession.stopRepeating();
-                mCaptureSession.close();
-                mCaptureSession = null;
-            }
-
-            mIsCameraConfigured = false;
-        } catch (final CameraAccessException e) {
-            // Doesn't matter, cloising device anyway
-            e.printStackTrace();
-        } catch (final IllegalStateException e2) {
-            // Doesn't matter, cloising device anyway
-            e2.printStackTrace();
-        } finally {
-            if (mCameraDevice != null) {
-                mCameraDevice.close();
-                mCameraDevice = null;
-                mCaptureSession = null;
-            }
-        }
-    }
-
-    @Override
-    public boolean handleMessage(Message msg) {
-        switch (msg.what) {
-            case MSG_CAMERA_OPENED:
-            case MSG_SURFACE_READY:
-                // if both surface is created and camera device is opened
-                // - ready to set up preview and other things
-                if (mSurfaceCreated && (mCameraDevice != null)
-                        && !mIsCameraConfigured) {
-                    configureCamera();
-                }
-                break;
-        }
-
-        return true;
-    }
-
-    private void configureCamera() {
-        // prepare list of surfaces to be used in capture requests
-        List<Surface> sfl = new ArrayList<Surface>();
-
-        sfl.add(mCameraSurface); // surface for viewfinder preview
-
-        // configure camera with all the surfaces to be ever used
-        try {
-            mCameraDevice.createCaptureSession(sfl,
-                    new CaptureSessionListener(), null);
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-
-        mIsCameraConfigured = true;
-    }
 
 
     @Override
@@ -285,54 +165,13 @@ public class CustomPreview extends AppCompatActivity implements SurfaceHolder.Ca
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_CAMERA:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                    try {
-                        mCameraManager.openCamera(mCameraIDsList[1], mCameraStateCB, new Handler());
-                    } catch (CameraAccessException e) {
-                        e.printStackTrace();
-                    }
+//                    try {
+//                        mCameraManager.openCamera(mCameraIDsList[1], mCameraStateCB, new Handler());
+//                    } catch (CameraAccessException e) {
+//                        e.printStackTrace();
+//                    }
                 break;
         }
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        mCameraSurface = holder.getSurface();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        mCameraSurface = holder.getSurface();
-        mSurfaceCreated = true;
-        mHandler.sendEmptyMessage(MSG_SURFACE_READY);
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        mSurfaceCreated = false;
-    }
-
-    private class CaptureSessionListener extends
-            CameraCaptureSession.StateCallback {
-        @Override
-        public void onConfigureFailed(final CameraCaptureSession session) {
-            Log.d(TAG, "CaptureSessionConfigure failed");
-        }
-
-        @Override
-        public void onConfigured(final CameraCaptureSession session) {
-            Log.d(TAG, "CaptureSessionConfigure onConfigured");
-            mCaptureSession = session;
-
-            try {
-                CaptureRequest.Builder previewRequestBuilder = mCameraDevice
-                        .createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-                previewRequestBuilder.addTarget(mCameraSurface);
-                mCaptureSession.setRepeatingRequest(previewRequestBuilder.build(),
-                        null, null);
-            } catch (CameraAccessException e) {
-                Log.d(TAG, "setting up preview failed");
-                e.printStackTrace();
-            }
-        }
-    }
 }
